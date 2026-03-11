4ffbcba0c2f53b82958a60c9a7fff9bd14ad353e
Jules Pabst <julia.pabst@kununu.com>
2026-03-11 14:28:35 +0100

    Add missing tests

--- apps/web/jest.setup.js
+++ apps/web/jest.setup.js
@@ -1,1 +1,5 @@
 import '@testing-library/jest-dom';
+
+// Set up required environment variables for tests
+process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://test.supabase.co';
+process.env.SUPABASE_SERVICE_ROLE_KEY = 'test-service-role-key';
